a780310baf7dcd07db4bcdec774f51acfaf53315
jbergleton <jbergleton>
2014-02-27 22:04:49 -0800

    Adding Cedar Specs (hope I don't regret this...)

---
+++ Specs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Specs
+//
+//  Created by Jessica Berglund on 2/27/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import <Cedar-iOS/Cedar-iOS.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    }
+}
